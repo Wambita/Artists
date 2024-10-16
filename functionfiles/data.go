fc53cb15d21a62a4a4f1be0dbb8e20e0ff213760
Tomlee Abila <tomleeabila@gmai.com>
2024-10-16 20:17:55 +0300

    fix: clean the code by removing unnecessary code

--- functionfiles/data.go
+++ functionfiles/data.go
@@ -4,7 +4,6 @@ import (
 	"encoding/json"
 	"log"
 	"net/http"
-	"sync"
 )
 
 // Api  urls
@@ -18,10 +17,6 @@ const (
 // gloabl vars for storing data
 var (
 	Artists   []Artist
-	Locations []Location
-	Dates     []Date
-	Relations []Relation
-	mu        sync.Mutex
 )
 
 // fetch json data form a url and decodes ot into target interface
@@ -36,56 +31,10 @@ func fetchData(url string, target interface{}) error {
 
 // loadData fetch data concurrently from all apis
 func LoadData() {
-	var wg sync.WaitGroup
-	wg.Add(4)
-
-	go func() {
-		defer wg.Done()
-		var ar []Artist
-		if err := fetchData(artistsURL, &ar); err != nil {
-			log.Println("Error fetching artist:", err)
-			return
-		}
-		mu.Lock()
-		Artists = ar
-		mu.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		var lr LocationsResponse
-		if err := fetchData(locationsURL, &lr); err != nil {
-			log.Println("Error fetching locations:", err)
-			return
-		}
-		mu.Lock()
-		Locations = lr.Locations
-		mu.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		var dr DatesResponse
-		if err := fetchData(datesURL, &dr); err != nil {
-			log.Println("Error fetching dates:", err)
-			return
-		}
-		mu.Lock()
-		Dates = dr.Dates
-		mu.Unlock()
-	}()
-
-	go func() {
-		defer wg.Done()
-		var rr RelationResponse
-		if err := fetchData(relationURL, &rr); err != nil {
-			log.Println("Error fetching relations:", err)
-			return
-		}
-		mu.Lock()
-		Relations = rr.Relation
-		mu.Unlock()
-	}()
-
-	wg.Wait()
+	var ar []Artist
+	if err := fetchData(artistsURL, &ar); err != nil {
+		log.Println("Error fetching artist:", err)
+		return
+	}
+	Artists = ar		
 }
