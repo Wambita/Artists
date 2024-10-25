f380ebefe6698bb207ae9022a0feb4b940d8d957
fnamayi <franklynenm@gmail.com>
2024-10-25 23:01:51 +0300

    feat: Improve template initialization efficiency
    - Implemented sync.Once to ensure templates are parsed only once.
    - Enhanced thread safety for concurrent calls to InitializeTemplates.
    - Reduced performance overhead by avoiding repeated parsing of templates.

--- functionfiles/utils.go
+++ functionfiles/utils.go
@@ -3,16 +3,22 @@ package groupie_tracker
 import (
 	"html/template"
 	"log"
+	"sync"
 )
 
-var Templates *template.Template
+var (
+	Templates *template.Template
+	once sync.Once //ensures initialization only happens once
+)
 
+// InitializeTemplates initializes the template variable, parsing all HTML files in the templates directory.
 func InitializeTemplates() {
+	once.Do(func (){
 	var err error
 	Templates, err = template.ParseGlob("templates/*.html")
 	if err != nil {
 		log.Fatalf("Error parsing templates: %v", err)
 	}
 
-	
+})
 }
