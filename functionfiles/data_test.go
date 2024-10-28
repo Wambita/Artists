c1f514579f5cadbc6e1273e0aa5e8b8617e0dd9b
Tomlee Abila <tomleeabila@gmail.com>
2024-10-29 00:04:54 +0300

    feat: add test for fetchData()

--- functionfiles/data_test.go
+++ functionfiles/data_test.go
@@ -7,8 +7,14 @@ import (
 	"testing"
 )
 
+type Artist1 struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Image string `json:"image"`
+}
+
 // Mock response for artists
-var mockArtistsResponse = []Artist{
+var mockArtistsResponse = []Artist1{
 	{ID: 1, Name: "Artist One", Image: "image1.jpg"},
 	{ID: 2, Name: "Artist Two", Image: "image2.jpg"},
 }
@@ -23,31 +29,6 @@ var mockLocationsResponse = struct {
 // Mock response for dates
 var mockDatesResponse = []string{"2024-10-30", "2024-11-05"}
 
-// // Test fetchData
-// func TestFetchData(t *testing.T) {
-// 	// Setup a mock server
-// 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		if r.URL.String() == artistsURL {
-// 			json.NewEncoder(w).Encode(mockArtistsResponse)
-// 		} else {
-// 			http.NotFound(w, r)
-// 		}
-// 	}))
-// 	defer server.Close()
-
-// 	// Update the URL to the mock server
-// 	artistsURL = server.URL
-
-// 	var artists []Artist
-// 	err := fetchData(artistsURL, &artists)
-// 	if err != nil {
-// 		t.Fatalf("Expected no error, got %v", err)
-// 	}
-
-// 	if len(artists) != len(mockArtistsResponse) {
-// 		t.Fatalf("Expected %d artists, got %d", len(mockArtistsResponse), len(artists))
-// 	}
-// }
 
 // Test LoadData
 func TestLoadData(t *testing.T) {
@@ -67,52 +48,108 @@ func TestLoadData(t *testing.T) {
 	}
 }
 
-// Test fetchLocations
-// func TestFetchLocations(t *testing.T) {
-// 	// Setup a mock server for locations
-// 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		if r.URL.String() == locationsURL {
-// 			json.NewEncoder(w).Encode(mockLocationsResponse)
-// 		} else {
-// 			http.NotFound(w, r)
-// 		}
-// 	}))
-// 	defer server.Close()
 
-// 	// Update the URL to the mock server
-// 	locationsURL = server.URL
+func TestFetchData(t *testing.T) {
+	// Set up a test server
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		switch r.URL.Path {
+		case "/artists":
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(mockArtistsResponse)
+		case "/locations":
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(mockLocationsResponse)
+		case "/dates":
+			w.WriteHeader(http.StatusOK)
+			json.NewEncoder(w).Encode(mockDatesResponse)
+		case "/notfound":
+			w.WriteHeader(http.StatusNotFound)
+		case "/invalidjson":
+			w.WriteHeader(http.StatusOK)
+			w.Write([]byte("invalid json"))
+		default:
+			w.WriteHeader(http.StatusNotFound)
+		}
+	}))
+	defer ts.Close()
 
-// 	locations, err := fetchLocations(locationsURL)
-// 	if err != nil {
-// 		t.Fatalf("Expected no error, got %v", err)
-// 	}
+	tests := []struct {
+		name       string
+		url        string
+		target     interface{}
+		wantErr    bool
+	}{
+		{
+			name:    "Fetch Artists",
+			url:     ts.URL + "/artists",
+			target:  &[]Artist1{},
+			wantErr: false,
+		},
+		{
+			name:    "Fetch Locations",
+			url:     ts.URL + "/locations",
+			target:  &struct{ Locations []string }{},
+			wantErr: false,
+		},
+		{
+			name:    "Fetch Dates",
+			url:     ts.URL + "/dates",
+			target:  &[]string{},
+			wantErr: false,
+		},
+		{
+			name:    "404 Not Found",
+			url:     ts.URL + "/notfound",
+			target:  &[]Artist1{},
+			wantErr: true,
+		},
+		{
+			name:    "Invalid JSON response",
+			url:     ts.URL + "/invalidjson",
+			target:  &[]Artist1{},
+			wantErr: true,
+		},
+	}
 
-// 	if len(locations) != len(mockLocationsResponse.Locations) {
-// 		t.Fatalf("Expected %d locations, got %d", len(mockLocationsResponse.Locations), len(locations))
-// 	}
-// }
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			err := fetchData(tt.url, tt.target)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("fetchData() error = %v, wantErr %v", err, tt.wantErr)
+			}
 
-// Test fetchDates
-// func TestFetchDates(t *testing.T) {
-// 	// Setup a mock server for dates
-// 	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-// 		if r.URL.String() == datesURL {
-// 			json.NewEncoder(w).Encode(mockDatesResponse)
-// 		} else {
-// 			http.NotFound(w, r)
-// 		}
-// 	}))
-// 	defer server.Close()
-
-// 	// Update the URL to the mock server
-// 	datesURL = server.URL
-
-// 	dates, err := fetchDates(datesURL)
-// 	if err != nil {
-// 		t.Fatalf("Expected no error, got %v", err)
-// 	}
-
-// 	if len(dates) != len(mockDatesResponse) {
-// 		t.Fatalf("Expected %d dates, got %d", len(mockDatesResponse), len(dates))
-// 	}
-// }
+			// If no error is expected, verify the data
+			if !tt.wantErr {
+				switch tt.url {
+				case ts.URL + "/artists":
+					var artists []Artist1
+					if err := fetchData(tt.url, &artists); err == nil {
+						if len(artists) != len(mockArtistsResponse) {
+							t.Errorf("Expected %d artists, got %d", len(mockArtistsResponse), len(artists))
+						}
+						
+						for i, artist := range artists {
+							if artist != mockArtistsResponse[i] {
+								t.Errorf("Expected artist %v, got %v", mockArtistsResponse[i], artists[i])
+							}
+						}
+					}
+				case ts.URL + "/locations":
+					var locations struct{ Locations []string }
+					if err := fetchData(tt.url, &locations); err == nil {
+						if len(locations.Locations) != len(mockLocationsResponse.Locations) {
+							t.Errorf("Expected %d locations, got %d", len(mockLocationsResponse.Locations), len(locations.Locations))
+						}
+					}
+				case ts.URL + "/dates":
+					var dates []string
+					if err := fetchData(tt.url, &dates); err == nil {
+						if len(dates) != len(mockDatesResponse) {
+							t.Errorf("Expected %d dates, got %d", len(mockDatesResponse), len(dates))
+						}
+					}
+				}
+			}
+		})
+	}
+}
