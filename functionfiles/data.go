7aff95225736c877ecb7e118cada1b8bb21e16d8
Tomlee Abila <tomleeabila@gmail.com>
2024-10-29 00:07:28 +0300

    refactor: remove unnecessary functions like fetchLocations() and fetchDates()

--- functionfiles/data.go
+++ functionfiles/data.go
@@ -9,9 +9,9 @@ import (
 // Api  urls
 var (
 	artistsURL   = "https://groupietrackers.herokuapp.com/api/artists"
-	locationsURL = "https://groupietrackers.herokuapp.com/api/locations"
-	datesURL     = "https://groupietrackers.herokuapp.com/api/dates"
-	relationURL  = "https://groupietrackers.herokuapp.com/api/relation"
+	LocationsURL = "https://groupietrackers.herokuapp.com/api/locations/"
+	DatesURL     = "https://groupietrackers.herokuapp.com/api/dates/"
+	RelationURL  = "https://groupietrackers.herokuapp.com/api/relation/"
 )
 
 // gloabl vars for storing data
@@ -27,7 +27,7 @@ func fetchData(url string, target interface{}) error {
 	}
 	defer resp.Body.Close()
 	return json.NewDecoder(resp.Body).Decode(target)
-}
+	}
 
 // loadData fetch data concurrently from all apis
 func LoadData() {
@@ -37,42 +37,4 @@ func LoadData() {
 		return
 	}
 	Artists = ar
-}
-
-// Function to fetch locations from the URL
-func fetchLocations(url string) ([]string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// Define a struct for the expected response
-	type LocationsResponse struct {
-		Locations []string `json:"locations"` // Adjust the JSON key based on your API response structure
-	}
-
-	var locationsResponse LocationsResponse
-	if err := json.NewDecoder(resp.Body).Decode(&locationsResponse); err != nil {
-		return nil, err
-	}
-
-	return locationsResponse.Locations, nil
-}
-
-// Function to fetch concert dates from the URL
-func fetchDates(url string) ([]string, error) {
-	resp, err := http.Get(url)
-	if err != nil {
-		return nil, err
-	}
-	defer resp.Body.Close()
-
-	// Define a struct for the expected response
-	var dates []string
-	if err := json.NewDecoder(resp.Body).Decode(&dates); err != nil {
-		return nil, err
-	}
-
-	return dates, nil
 }
