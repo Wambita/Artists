4fc2961b1ceb2cbb68b7f9c2a6c2e87454e4240c
shfana <wambitafana@gmail.com>
2024-10-29 12:50:18 +0300

    Fix: add static file handler

--- main.go
+++ main.go
@@ -21,7 +21,7 @@ func main() {
 	http.HandleFunc("/artist", groupie_tracker.ArtistHandler)
 
 	// Serve static files
-	http.HandleFunc("/static/", groupie_tracker.RouteHandler)
+	http.HandleFunc("/static/", groupie_tracker.StaticFileHandler)
 
 	// Start server
 	port := os.Getenv("PORT")
