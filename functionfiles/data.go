a23e7717a8f044962d47e994b3fdb87ba0a132bb
Tomlee Abila <tomleeabila@gmai.com>
2024-11-04 14:27:19 +0300

    feat: load every data to artist

--- functionfiles/data.go
+++ functionfiles/data.go
@@ -1,6 +1,8 @@
 package groupie_tracker
 
 import (
+	"fmt"
+	"strconv"
 	"encoding/json"
 	"log"
 	"net/http"
@@ -8,10 +10,11 @@ import (
 
 // Api  urls
 // var (
-	var artistsURL   = "https://groupietrackers.herokuapp.com/api/artists"
-	var LocationsURL = "https://groupietrackers.herokuapp.com/api/locations/"
-	var DatesURL     = "https://groupietrackers.herokuapp.com/api/dates/"
-	var RelationURL  = "https://groupietrackers.herokuapp.com/api/relation/"
+var artistsURL = "https://groupietrackers.herokuapp.com/api/artists"
+var LocationsURL = "https://groupietrackers.herokuapp.com/api/locations/"
+var DatesURL = "https://groupietrackers.herokuapp.com/api/dates/"
+var RelationURL = "https://groupietrackers.herokuapp.com/api/relation/"
+
 // )
 
 // gloabl vars for storing data
@@ -36,5 +39,58 @@ func LoadData() {
 		log.Println("Error fetching artist:", err)
 		return
 	}
+
 	Artists = ar
+
+	for i := range Artists {
+		artistID := strconv.Itoa(Artists[i].ID)
+
+		// Define a struct for the expected data structure
+		type datesLocations struct {
+			DatesLocations map[string][]string `json:"datesLocations"`
+		}
+
+		// Define a struct for the expected data structure
+		type concertDates struct {
+			ConcertDates []string `json:"dates"`
+		}
+
+		// Define a struct for the expected data structure
+		type Locations struct {
+			Locations []string `json:"locations"`
+		}
+
+		var reletions datesLocations
+
+		var locations Locations
+
+		var dates concertDates
+
+		fmt.Println(artistID)
+
+		// artist
+		if err := fetchData(RelationURL+artistID, &reletions); err != nil {
+			// ErrorHandler(w, r, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Println(err)
+			return
+		}
+		Artists[i].DatesLocations = reletions.DatesLocations
+
+	
+		if err := fetchData(LocationsURL+artistID, &locations); err != nil {
+			// ErrorHandler(w, r, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Println(err)
+			return
+		}
+		Artists[i].Locations = locations.Locations
+
+	
+		if err := fetchData(DatesURL+artistID, &dates); err != nil {
+			// ErrorHandler(w, r, "Internal Server Error", http.StatusInternalServerError)
+			fmt.Println(err)
+			return
+		}
+		Artists[i].ConcertDates = dates.ConcertDates
+		
+	}
 }
